0be118876bbe05013f8903169d8c6f59d5fd38e7
Praveen Bommali <65863196+praveenbommalibits@users.noreply.github.com>
2024-01-08 22:09:08 +0530

    Initial commit

---
+++ src/main/java/com/praveen/bookmyshow/BookmyshowAppApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.bookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmyshowAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmyshowAppApplication.class, args);
+	}
+
+}
